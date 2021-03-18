338dba81709bf267dd673b8323d2df041105a881
Andrew Tran <andtran0703@gmail.com>
2021-03-18 10:54:34 -0400

    potential fix to change message from dashboard

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ def cron_process():
 	x = datetime.datetime.utcnow()
 	print('Function fired successfully!\n')
 	if (x.hour == 3 or x.hour == 9 or x.hour == 23) and x.minute == 0:
-		webhook = DiscordWebhook(url=str(os.environ.get("serverurl")), content='@everyone, Elusive Planet Leonard is open for the next hour!')
+		webhook = DiscordWebhook(url=str(os.environ.get("serverurl")), content=str(os.environ.get("message")))
 		response = webhook.execute()
 		print('Message printed!\n')
 
